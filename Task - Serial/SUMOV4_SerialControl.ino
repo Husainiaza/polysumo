c66a6f5a970d9ebca469f87614aecaeadcd60715
Husainiaza <148662620+Husainiaza@users.noreply.github.com>
2023-10-31 17:04:42 +0800

    Add files via upload

---
+++ Task - Serial/SUMOV4_SerialControl.ino
@@ -0,0 +1,70 @@
+const int motorkiriA1 = 17 ; 
+const int motorkiriA2 = 12 ; 
+const int motorkananB1 = 18 ; 
+const int motorkananB2 = 19 ; 
+const int buzzer = 25 ;  
+
+void setup() {
+
+//-- Motor KIRI
+  pinMode(motorkiriA1, OUTPUT);
+  pinMode(motorkiriA2, OUTPUT);
+//-- Motor Kanan  F
+  pinMode(motorkananB1, OUTPUT);
+  pinMode(motorkananB2, OUTPUT);
+//--  buzzer
+  pinMode(buzzer, OUTPUT);
+
+//-- istihar fungsi komunikasi Serial.
+  Serial.begin(115200);
+  delay(100);
+  Serial.println(" Sistem Robot Mula");
+}
+
+void loop() {
+
+  if(Serial.available()> 0){
+    char dat = Serial.read();
+    Serial.println(dat);
+    
+    if (dat=='B'){
+      digitalWrite(motorkiriA1,HIGH);
+      digitalWrite(motorkiriA2,LOW);
+      digitalWrite(motorkananB1,LOW);
+      digitalWrite(motorkananB2,HIGH);
+      Serial.println(" Motor Gerak ke UNDUR");
+      
+    }
+    
+    else if (dat=='F'){
+      digitalWrite(motorkiriA1,LOW);
+      digitalWrite(motorkiriA2,HIGH);
+      digitalWrite(motorkananB1,HIGH);
+      digitalWrite(motorkananB2,LOW);
+      Serial.println(" Motor Gerak ke DEPAN ");
+    }
+    else if (dat=='S'){
+      digitalWrite(motorkiriA1,LOW);
+      digitalWrite(motorkiriA2,LOW);
+      digitalWrite(motorkananB1,LOW);
+      digitalWrite(motorkananB2,LOW);
+      Serial.println(" Motor HENTI");
+    }
+
+    else if (dat=='L'){
+      digitalWrite(motorkiriA1,LOW);
+      digitalWrite(motorkiriA2,HIGH);
+      digitalWrite(motorkananB1,LOW);
+      digitalWrite(motorkananB2,HIGH);
+      Serial.println(" Motor Gerak ke KIRI");
+    }
+
+    else if (dat=='R'){
+      digitalWrite(motorkiriA1,HIGH);
+      digitalWrite(motorkiriA2,LOW);
+      digitalWrite(motorkananB1,HIGH);
+      digitalWrite(motorkananB2,LOW);
+      Serial.println(" Motor Gerak ke KANAN");
+    }
+  }
+}
