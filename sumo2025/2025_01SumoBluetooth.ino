52ce6ce58ddbcb08ee26833cc351e56f6f330748
Husainiaza <148662620+Husainiaza@users.noreply.github.com>
2025-04-23 23:23:25 +0800

    Create 2025_01SumoBluetooth.ino

---
+++ sumo2025/2025_01SumoBluetooth.ino
@@ -0,0 +1,62 @@
+#include "BluetoothSerial.h"
+
+#define LED_PIN     5
+#define NUM_LEDS    8
+#define mtrA01      17
+#define mtrA02      12
+#define mtrB01      18
+#define mtrB02      19
+#define buzzer      25  
+#define lampu       15
+
+BluetoothSerial my_bluetooth;
+const char* nama_bluetooth = "KKSJ-01";
+
+
+void setup() {
+
+//-- Motor KIRI
+pinMode(mtrA01, OUTPUT);
+pinMode(mtrA02, OUTPUT);
+//-- Motor Kanan  F
+pinMode(18, OUTPUT);
+pinMode(mtrB02, OUTPUT);
+pinMode(buzzer, OUTPUT);
+ 
+Serial.begin(115200);
+delay(100);
+
+my_bluetooth.begin(nama_bluetooth);
+
+}
+
+void loop() {
+
+  if(my_bluetooth.available()> 0){
+    //char dat = Serial.read();
+    char dat = my_bluetooth.read();
+    Serial.println(dat);
+    
+    if (dat=='B'){
+      digitalWrite(mtrA01,HIGH);
+      digitalWrite(mtrA02,LOW);
+      digitalWrite(18,LOW);
+      digitalWrite(mtrB02,HIGH);
+      Serial.println(" Motor Gerak ke UNDUR");
+      
+    }
+    
+    if (dat=='F'){
+      digitalWrite(mtrA01,LOW);
+      digitalWrite(mtrA02,HIGH);
+      digitalWrite(18,HIGH);
+      digitalWrite(mtrB02,LOW);
+      Serial.println(" Motor Gerak ke DEPAN ");
+    }
+    if (dat=='S'){
+      digitalWrite(mtrA01,LOW);
+      digitalWrite(mtrA02,LOW);
+      digitalWrite(18,LOW);
+      digitalWrite(mtrB02,LOW);
+      Serial.println(" Motor HENTI");
+    }
