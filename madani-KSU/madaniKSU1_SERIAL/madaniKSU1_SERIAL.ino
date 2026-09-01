2fae62af00fe7e039c717de5bc773dff723a2a58
Husainiaza <148662620+Husainiaza@users.noreply.github.com>
2026-09-01 17:33:44 +0800

    Add files via upload

---
+++ madani-KSU/madaniKSU1_SERIAL/madaniKSU1_SERIAL.ino
@@ -0,0 +1,106 @@
+/*-------------- SUMO ROBOT BOARD VER V4------------------------
+  ===================  Seksyen 1 - HEADER        ===================
+  ------------------------------------------------------------------*/
+//-------------------------------------------------------------------
+//=  A. - Library  - #include and #define  yang diperlukan          =
+//-------------------------------------------------------------------
+
+//-------------------------------------------------------------------
+//=  B. - ISTIHAR Sambungan nama Pin dengan Nombor Fizikal PIN      =
+//-------------------------------------------------------------------
+const int motorkiriA1  = 17 ; 
+const int motorkiriA2  = 12 ; 
+const int motorkananB1 = 18 ; 
+const int motorkananB2 = 19 ; 
+const int buzzer = 33 ;  
+
+//##################  Seksyen 1 - TAMAT #############################
+
+
+//==================  Seksyen 2 - Fungsi Setup ======================
+//-------------------------------------------------------------------
+void setup() {
+
+ //-- Motor KIRI
+  pinMode(motorkiriA1, OUTPUT);
+  pinMode(motorkiriA2, OUTPUT);
+ //-- Motor Kanan  F
+  pinMode(motorkananB1, OUTPUT);
+  pinMode(motorkananB2, OUTPUT);
+ //--  buzzer
+  pinMode(buzzer, OUTPUT);
+
+ //-- istihar fungsi komunikasi Serial.
+  Serial.begin(115200);
+  delay(100);
+  Serial.println(" Sistem Robot Mula");
+}
+
+//##################  Seksyen 2 - TAMAT #############################
+
+
+//==============  Seksyen 3 - Fungsi Utama (LOOP) ===================
+//-------------------------------------------------------------------
+void loop() {
+
+  if(Serial.available()> 0){
+    char dat = Serial.read();
+    Serial.println(dat);
+    
+
+    //---Kawalan Motor Gerak ke DEPAN --
+    if (dat=='F'){
+      digitalWrite(motorkiriA1,HIGH);
+      digitalWrite(motorkiriA2,LOW);
+      digitalWrite(motorkananB1,LOW);
+      digitalWrite(motorkananB2,HIGH);
+      Serial.println(" Motor Gerak ke DEPAN");
+    }
+    
+    //---Kawalan Motor Gerak ke Depan --
+    else if (dat=='B'){
+      digitalWrite(motorkiriA1,LOW);
+      digitalWrite(motorkiriA2,HIGH);
+      digitalWrite(motorkananB1,HIGH);
+      digitalWrite(motorkananB2,LOW);
+      Serial.println(" Motor Gerak ke UNDUR ");
+    }
+
+    //---Kawalan Motor Henti --
+    else if (dat=='S'){
+      digitalWrite(motorkiriA1,LOW);
+      digitalWrite(motorkiriA2,LOW);
+      digitalWrite(motorkananB1,LOW);
+      digitalWrite(motorkananB2,LOW);
+      Serial.println(" Motor HENTI");
+    }
+
+   //---Kawalan Motor Gerak ke KIRI --
+    else if (dat=='L'){
+      digitalWrite(motorkiriA1,LOW);
+      digitalWrite(motorkiriA2,HIGH);
+      digitalWrite(motorkananB1,LOW);
+      digitalWrite(motorkananB2,HIGH);
+      Serial.println(" Motor Gerak ke KIRI");
+    }
+
+   //---Kawalan Motor Gerak ke KANAN --
+    else if (dat=='R'){
+      digitalWrite(motorkiriA1,HIGH);
+      digitalWrite(motorkiriA2,LOW);
+      digitalWrite(motorkananB1,HIGH);
+      digitalWrite(motorkananB2,LOW);
+      Serial.println(" Motor Gerak ke KANAN");
+    }
+
+    else if (dat=='H'){
+      digitalWrite(buzzer,HIGH);
+      Serial.println(" buzzer bunyi");
+    }
+
+     else if (dat=='h'){
+      digitalWrite(buzzer,LOW);
+      Serial.println(" buzzer senyap");
+    }
+  }
+}
